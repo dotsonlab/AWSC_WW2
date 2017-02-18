b5ddebf83c65ee091c37f305820c93107bcab419
Aaron Dotson <addotson@uaa.alaska.edu>
2017-02-18 14:03:34 -0900

    Update guikeys.py

--- guikeys.py
+++ guikeys.py
@@ -10,8 +10,37 @@ def do_something():
         print "Hi Mom!"
         time.sleep(.1)
 
-def _run():
-    print "Work?!"
+def _RegularDay():
+    print "Regular Day Treatment"
+    #ser.write(cmd)
+
+def _WasteDay():
+    print "Waste Day Treatment"
+    #ser.write(cmd)
+
+def _CartridgeFilter():
+    print "Cartridge Filter"
+    #ser.write(cmd)
+
+def _CartridgeFilterWO():
+    print "Cartridge Filter with Rinse"
+    #ser.write(cmd)
+
+def _Nanofilter():
+    print "Nanofilter"
+    #ser.write(cmd)
+
+def _NanofilterWO():
+    print "Nanofilter without Rinse"
+    #ser.write(cmd)
+
+def _ReverseOsmosis():
+    print "Reverse Osmosis"
+    #ser.write(cmd)
+
+def _ReverseOsmosisWO():
+    print "Reverse Osmosis without Rinse"
+    #ser.write(cmd)
 
 def _quit():
     print 'Exiting...'
@@ -21,10 +50,25 @@ def _quit():
     root.destroy()
 
 root = Tk.Tk()
+RegButton = Tk.Button(master=root, text='Regular Day Treatment', command=_RegularDay) #the quit button
+RegButton.grid(row=0, column=3)
+WasteButton = Tk.Button(master=root, text='Waste Day Treatment', command=_WasteDay) #the quit button
+WasteButton.grid(row=1, column=3)
 QuitButton = Tk.Button(master=root, text='Quit', command=_quit) #the quit button
-QuitButton.pack(side=Tk.BOTTOM)
-RunButton = Tk.Button(master=root, text='Run', command=_run)
-RunButton.pack(side=Tk.BOTTOM)
+QuitButton.grid(row=5, column=2)
+CFButton = Tk.Button(master=root, text='Cartridge Filter', command=_CartridgeFilter)
+CFButton.grid(row=0, column=0)
+CFWOButton = Tk.Button(master=root, text='Cartridge Filter without Rinse', command=_CartridgeFilterWO)
+CFWOButton.grid(row=0, column=1)
+NFButton = Tk.Button(master=root, text='Nanofilter', command=_Nanofilter)
+NFButton.grid(row=1, column=0)
+NFWOButton = Tk.Button(master=root, text='Nanofilter without Rinse', command=_NanofilterWO)
+NFWOButton.grid(row=1, column=1)
+ROButton = Tk.Button(master=root, text='Reverse Osomisis', command=_ReverseOsmosis)
+ROButton.grid(row=2, column=0)
+ROWOButton = Tk.Button(master=root, text='Reverse Osmosis without Rinse', command=_ReverseOsmosisWO)
+ROWOButton.grid(row=2, column=1)
+
 
 thread = threading.Thread(target=do_something, args=())
 e = threading.Event()
