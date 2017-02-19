6dbccb3da45c1d670196ff44789dd42e22a53834
Aaron Dotson <addotson@alaska.edu>
2017-02-18 20:27:57 -0900

    updated gui with scheduler

--- guikeys.py
+++ guikeys.py
@@ -1,20 +1,22 @@
 import time
+import schedule  #will have to pip install schedule (library)
 import threading
 import Tkinter as Tk
 
-def do_something():
-    T0 = time.clock()
-    while (time.clock() - T0) < 60 and not e.isSet(): #as long as 60s haven't elapsed
-                                                      #and the flag is not set
-        #here do a bunch of stuff
-        print "Hi Mom!"
+def RunAndLog():
+    while not e.isSet():
+        schedule.run_pending()
+        #print "I will be datalogging and providing initiation commands here!"
         time.sleep(.1)
 
-def _RegularDay():
+def TreatmentTimer():
+    print "set timer here"
+
+def _RegularDay(t):
     print "Regular Day Treatment"
     #ser.write(cmd)
 
-def _WasteDay():
+def _WasteDay(t):
     print "Waste Day Treatment"
     #ser.write(cmd)
 
@@ -50,27 +52,52 @@ def _quit():
     root.destroy()
 
 root = Tk.Tk()
-RegButton = Tk.Button(master=root, text='Regular Day Treatment', command=_RegularDay) #the quit button
-RegButton.grid(row=0, column=3)
-WasteButton = Tk.Button(master=root, text='Waste Day Treatment', command=_WasteDay) #the quit button
-WasteButton.grid(row=1, column=3)
+root.wm_title("WWT Control")
+RegButton = Tk.Button(master=root, text='Regular Day', command=_RegularDay) #the quit button
+RegButton.grid(row=0, column=4)
+RegButton.config(height = 2, width = 8)
+
+WasteButton = Tk.Button(master=root, text='Waste Day', command=_WasteDay) #the quit button
+WasteButton.grid(row=1, column=4)
+WasteButton.config(height = 2, width = 8)
+
 QuitButton = Tk.Button(master=root, text='Quit', command=_quit) #the quit button
-QuitButton.grid(row=5, column=2)
-CFButton = Tk.Button(master=root, text='Cartridge Filter', command=_CartridgeFilter)
+QuitButton.grid(row=5, column=4)
+QuitButton.config(height = 2, width = 8)
+
+CFButton = Tk.Button(master=root, text='CF', command=_CartridgeFilter)
 CFButton.grid(row=0, column=0)
-CFWOButton = Tk.Button(master=root, text='Cartridge Filter without Rinse', command=_CartridgeFilterWO)
-CFWOButton.grid(row=0, column=1)
-NFButton = Tk.Button(master=root, text='Nanofilter', command=_Nanofilter)
+CFButton.config(height = 2, width = 8)
+
+CFWOButton = Tk.Button(master=root, text='CF wo R', command=_CartridgeFilterWO)
+CFWOButton.grid(row=0, column=2)
+CFWOButton.config(height = 2, width = 8)
+
+NFButton = Tk.Button(master=root, text='NF', command=_Nanofilter)
 NFButton.grid(row=1, column=0)
-NFWOButton = Tk.Button(master=root, text='Nanofilter without Rinse', command=_NanofilterWO)
-NFWOButton.grid(row=1, column=1)
-ROButton = Tk.Button(master=root, text='Reverse Osomisis', command=_ReverseOsmosis)
+NFButton.config(height = 2, width = 8)
+
+NFWOButton = Tk.Button(master=root, text='NF wo R', command=_NanofilterWO)
+NFWOButton.grid(row=1, column=2)
+NFWOButton.config(height = 2, width = 8)
+
+ROButton = Tk.Button(master=root, text='RO', command=_ReverseOsmosis)
 ROButton.grid(row=2, column=0)
-ROWOButton = Tk.Button(master=root, text='Reverse Osmosis without Rinse', command=_ReverseOsmosisWO)
-ROWOButton.grid(row=2, column=1)
+ROButton.config(height = 2, width = 8)
 
+ROWOButton = Tk.Button(master=root, text='RO wo R', command=_ReverseOsmosisWO)
+ROWOButton.grid(row=2, column=2)
+ROWOButton.config(height = 2, width = 8)
 
-thread = threading.Thread(target=do_something, args=())
+schedule.every().monday.at("9:00").do(_RegularDay,'It is 9:00 on a Regular Treatment Day')
+schedule.every().tuesday.at("9:00").do(_WasteDay,'It is 9:00 on a Waste Treatment Day')
+schedule.every().wednesday.at("9:00").do(_RegularDay,'It is 9:00 on a Regular Treatment Day')
+schedule.every().thursday.at("9:00").do(_RegularDay,'It is 9:00 on a Regular Treatment Day')
+schedule.every().friday.at("9:00").do(_RegularDay,'It is 9:00 on a Regular Treatment Day')
+schedule.every().saturday.at("20:23").do(_RegularDay,'It is 9:00 on a Regular Treatment Day')
+schedule.every().sunday.at("9:00").do(_RegularDay,'It is 9:00 on a Regular Treatment Day')
+
+thread = threading.Thread(target=RunAndLog, args=())
 e = threading.Event()
 thread.start()
 root.mainloop()
