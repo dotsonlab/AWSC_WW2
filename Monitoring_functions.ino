6c18d6c543cf89f35885854d9f825e013d6c6264
greg8693 <gnmichaelson@alaska.edu>
2017-02-21 12:04:39 -0900

    updated equation for wwtank

--- Monitoring_functions.ino
+++ Monitoring_functions.ino
@@ -249,7 +249,7 @@ void setup() {
    junk = analogRead(preftankpin);
   spretank = analogRead(preftankpin)*0.668388-146.32479;
    junk = analogRead(wwtankpin);
-  swwtank = analogRead(wwtankpin)*0.65284-134.907;
+  swwtank = analogRead(wwtankpin)*0.65375348-134.42675;
    junk = analogRead(wastetankpin);
   swastetank = (analogRead(wastetankpin)*0.14351-28.702)*24*18*0.004329;
    junk = analogRead(ropotpin);
@@ -631,7 +631,7 @@ void tanklevel(){//read and average tank levels for all 5 tanks
   float pretank = analogRead(preftankpin)*0.668388-146.32479;
 
   junk = analogRead(wwtankpin);
-  float wwtank = analogRead(wwtankpin)*0.65284-134.907;
+  float wwtank = analogRead(wwtankpin)*0.65375348-134.42675;
 
   junk = analogRead(wastetankpin);
   float wastetank = (analogRead(wastetankpin)*0.14351-28.702)*24*18*0.004329;
@@ -726,7 +726,9 @@ if (systemstate ==1){
 
   if (t - o3starttime >= o3time && ozonestatus ==1){
   o3(0);
-  delay(60000);
+  delay(20000);
+  delay(20000);
+  delay(20000);
   o3pump(0);
 } }}
 void waiting(unsigned long interval){//function to read and report everything at given intervals
@@ -1202,7 +1204,7 @@ void PRE(int target, int rinsecycle){
   waiting(1);
   bw = millis();
   float PREflows=flw[4];
-  while (snfftank< target && spretank> 5){
+  while (snfftank< target && spretank> 7){
     //Serial.print(flowtarget);Serial.print("  "); Serial.println(flw[4]-PREflows);
     waiting(10000);
     lcd.setCursor(0, 3);
