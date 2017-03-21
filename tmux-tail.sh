0dc64699a7aaa7b6d517057d50f18a6a4d244f97
Aaron Dotson <addotson@uaa.alaska.edu>
2017-03-21 15:58:14 -0800

    Update tmux-tail.sh

--- tmux-tail.sh
+++ tmux-tail.sh
@@ -6,9 +6,10 @@ tmux new -s tail_log -d
 tmux send-keys -t tail_log 'tail -f 2017_3_21_WWT-Pressure.txt' C-m
 tmux split-window -v -t tail_log
 tmux send-keys -t tail_log 'tail -f 2017_3_21_WWT-TankLevels.txt' C-m
-tmux split-window -h -t tail_log
-tmux send-keys -t tail_log 'tail -f 2017_3_21_WWT-iFlows.txt' C-m
+tmux split-window -v -t tail_log
+tmux send-keys -t tail_log 'tail -f 2017_3_21_WWT-iFlow.txt' C-m
 tmux split-window -v -t tail_log
 tmux send-keys -t tail_log 'tail -f 2017_3_21_WWT-Relays.txt' C-m
+tmux select-layout -t tail_log tiled
 tmux attach -t tail_log
 
