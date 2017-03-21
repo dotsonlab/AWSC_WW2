f06ee844f07a162078e977dee7cf79e2301a72aa
Aaron Dotson <addotson@uaa.alaska.edu>
2017-03-21 15:52:11 -0800

    Update tmux-tail.sh

--- tmux-tail.sh
+++ tmux-tail.sh
@@ -3,5 +3,5 @@
 cd /home/pi/github/AWSC_WW2
 
 tmux new -s tail_log -d
-tmux send-keys -t tail_log 'tail -f 2017_3_21_WWT_Pressure.txt' C-m
+tmux send-keys -t tail_log 'tail -f 2017_3_21_WWT-Pressure.txt' C-m
 tmux attach -t tail_log
