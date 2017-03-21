ce400eff2baddd9f2395cab986a7d278b2cf3fa0
Aaron Dotson <addotson@uaa.alaska.edu>
2017-03-21 15:51:34 -0800

    revised

--- tmux-tail.sh
+++ tmux-tail.sh
@@ -2,13 +2,6 @@
 
 cd /home/pi/github/AWSC_WW2
 
-tmux new-session -d -s 'tail -f 2017_3_21_WWT_Pressure.txt'
-#tmux send-keys 'bundle exec thin start' 'C-m'
-#tmux rename-window 'Foo'
-#tmux select-window -t foo:0
-#tmux split-window -h 'exec pfoo'
-#tmux send-keys 'bundle exec compass watch' 'C-m'
-#tmux split-window -v -t 0 'exec pfoo'
-#tmux send-keys 'rake ts:start' 'C-m'
-#tmux split-window -v -t 1 'exec pfoo'
-#tmux -2 attach-session -t foo
+tmux new -s tail_log -d
+tmux send-keys -t tail_log 'tail -f 2017_3_21_WWT_Pressure.txt' C-m
+tmux attach -t tail_log
