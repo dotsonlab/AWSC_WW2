28f80f5a27f64e7dae0b4af0869d8106815e29ad
Aaron Dotson <addotson@uaa.alaska.edu>
2017-03-21 16:10:28 -0800

    Update tmux-tail.sh

--- tmux-tail.sh
+++ tmux-tail.sh
@@ -10,6 +10,9 @@ tmux split-window -v -t tail_log
 tmux send-keys -t tail_log 'tail -f 2017_3_21_WWT-iFlow.txt' C-m
 tmux split-window -v -t tail_log
 tmux send-keys -t tail_log 'tail -f 2017_3_21_WWT-Relays.txt' C-m
+tmux split-window -v -t tail_log
+tmux send-keys -t tail_log 'tail -f 2017_3_21_WWT-TandPD.txt' C-m
+tmux split-window -v -t tail_log
 tmux select-layout -t tail_log tiled
 tmux attach -t tail_log
 
