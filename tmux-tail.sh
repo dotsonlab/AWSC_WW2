cfd8e77f2e33000d73e3ab79e76a3732d18f638b
Aaron Dotson <addotson@uaa.alaska.edu>
2017-03-21 16:37:15 -0800

    Update tmux-tail.sh

--- tmux-tail.sh
+++ tmux-tail.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-DATE = date "+%Y_%-m_%-d"
+TODAY = date "+%Y_%-m_%-d"
 
 cd /home/pi/github/AWSC_WW2
 
 tmux new -s tail_log -d
-tmux send-keys -t tail_log 'tail -f $DATE_WWT-Pressure.txt' C-m
+tmux send-keys -t tail_log 'tail -f '$TODAY_WWT-Pressure.txt'' C-m
 tmux split-window -v -t tail_log
-tmux send-keys -t tail_log 'tail -f $DATE_WWT-TankLevels.txt' C-m
+tmux send-keys -t tail_log 'tail -f '$TODAY_WWT-TankLevels.txt'' C-m
 tmux split-window -v -t tail_log
-tmux send-keys -t tail_log 'tail -f $DATE_WWT-iFlow.txt' C-m
+tmux send-keys -t tail_log 'tail -f '$TODAY_WWT-iFlow.txt'' C-m
 tmux split-window -v -t tail_log
-tmux send-keys -t tail_log 'tail -f $DATE_WWT-Relays.txt' C-m
+tmux send-keys -t tail_log 'tail -f '$TODAY_WWT-Relays.txt'' C-m
 tmux split-window -h -t tail_log
-tmux send-keys -t tail_log 'tail -f "$DATE_WWT-TandPD.txt' C-m
+tmux send-keys -t tail_log 'tail -f '$TODAY_WWT-TandPD.txt'' C-m
 tmux split-window -v -t tail_log
 tmux select-layout -t tail_log tiled
 tmux attach -t tail_log
